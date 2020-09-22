59d05a4936dcd62dcddb44389d6aee0b7ec142e0
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-22 01:00:36 +0000

    Automatically backed up by Learn

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -12,7 +12,7 @@ class Pancake extends React.Component {
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
 componentDidMount(){
-  this.startInterval
+  this.interval = setInterval(this.updateCounter, 1000);
 }
   // TODO: create a componentWillUnmount() which will clear the interval
 componentWillUnmount(){
